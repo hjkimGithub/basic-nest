d092e2cab53851ede21dfca85173785026163fea
김현재 <rlaguswo3408@gmail.com>
2022-12-15 13:16:21 +0900

    simple movie api2

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -8,23 +8,31 @@ export class MoviesController {
         return 'This will return all movies';
     } 
 
-    @Get("/:id")
+    @Get("search")
+    search(@Query("year") searchingYear: string) {
+        return `We are searching for a movie with title made after: ${searchingYear}`;
+    }
+
+    @Get(":id")
     getOne(@Param("id") id: string) {
         return `This will return movie with id ${id}`;
     }
 
     @Post()
-    create() {
-        return 'This will create a movie';
+    create(@Body() movieData) {
+        return movieData;
     }
 
-    @Delete("/:id")
+    @Delete(":id")
     remove(@Param('id') id: string) {
         return `This will delete a movie ${id}`;
     }
 
-    @Patch("/:id")
-    path(@Param('id') id:string) {
-        return `This will Patch a movie ${id}`;
+    @Patch(":id")
+    path(@Param('id') id:string, @Body() updateData) {
+        return {
+            updatedMovie: id,
+            ...updateData,
+        }
     }
 }
