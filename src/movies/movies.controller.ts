465eba4c180feace3bab67777d042554ebd2d050
김현재 <rlaguswo3408@gmail.com>
2022-12-19 19:53:23 +0900

    simple movie api(dto)

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -12,22 +14,22 @@ export class MoviesController {
     } 
 
     @Get(":id")
-    getOne(@Param("id") movieId: string): Movie {
+    getOne(@Param("id") movieId: number): Movie {
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         return this.moviesService.create(movieData);
     }
 
     @Delete(":id")
-    remove(@Param('id') movieId: string) {
+    remove(@Param('id') movieId: number) {
         return this.moviesService.deleteOne(movieId)
     }
 
     @Patch(":id")
-    path(@Param('id') movieId:string, @Body() updatedData) {
+    path(@Param('id') movieId:number, @Body() updatedData: UpdateMovieDto) {
         return this.moviesService.update(movieId, updatedData)
     }
 }
