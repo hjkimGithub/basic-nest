79beee2f429a7d070dec9c55681d87538cbf08d5
김현재 <rlaguswo3408@gmail.com>
2022-12-21 18:04:09 +0900

    test2(e2e)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { ignoreElements } from 'rxjs';
@@ -7,12 +7,19 @@ import { ignoreElements } from 'rxjs';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    // 테스트와 운영은 환경 동일해야 함
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist : true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }));
+
     await app.init();
   });
 
@@ -48,5 +55,20 @@ describe('AppController (e2e)', () => {
         // NOT FOUND
         .expect(404)
     })
+  })
+
+  describe('/movies/:id', () => {
+    it("GET 200", () => {
+      return request(app.getHttpServer())
+        .get("/movies/1")
+        .expect(200)
+    })
+    it("GET 404", () => {
+      return request(app.getHttpServer())
+        .get("/movies/9999")
+        .expect(404)
+    })
+    it.todo("DELETE")
+    it.todo("PATCH")
   })
 });
